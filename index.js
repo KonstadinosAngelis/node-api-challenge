ed73f063cdc790c340b83080a5ef0008f4e15a68
Konstadinos Angelis <angeliskonstadinos@gmail.com>
2020-02-14 09:28:36 -0800

    finished MVP

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-/*
-play this: https://www.youtube.com/watch?v=d-diB65scQU
+const express = require('express');
+const projects = require('./routes/projectsRoute');
+const actions = require('./routes/actionsRoute');
 
-Sing along:
+const server = express();
 
-here's a little code I wrote, please read the README word for word, don't worry, you got this
-in every task there may be trouble, but if you worry you make it double, don't worry, you got this
-ain't got no sense of what is REST? just concentrate on learning Express, don't worry, you got this
-your file is getting way too big, bring a Router and make it thin, don't worry, be crafty
-there is no data on that route, just write some code, you'll sort it out… don't worry, just hack it…
-I need this code, but don't know where, perhaps should make some middleware, don't worry, just hack it
+server.use(logger)
+server.use(express.json())
+server.use('/api/projects', (projects))
+server.use('/api/actions', (actions))
 
-Go code!
-*/
+
+
+function logger(req, res, next) {
+  console.log(`${req.method} to ${req.originalUrl}`)
+  next();
+}
+
+server.listen(5000, () => {
+  console.log("Server is listening on port 5000")
+})
